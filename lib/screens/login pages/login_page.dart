daf24efb630828cf1ac62b7f6cbf07b40072c07b
Noah Lee <noahjefferson.lee@calbaptist.edu>
2024-11-06 13:09:31 -0800

    Added basic functionality of a log in page. Broke out separate widgets for logging in and returning values

---
+++ lib/screens/login pages/login_page.dart
@@ -0,0 +1,61 @@
+import 'package:csc322_streaker_final/screens/login%20pages/sign_in.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key, required this.doLogin});
+
+  final void Function() doLogin;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final Color menuStyle = Colors.white;
+  //Controllers for getting email and password data
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  //focus nodes for email and password fields
+
+  //empty variables for email and password
+  String _email = '';
+  String _password = '';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _checkLogin() {
+    //Empty placeholder to print values to the debug console.
+    //Later, this should check firebase for login status
+    _email = _emailController.text;
+    _password = _passwordController.text;
+    print('Email: $_email, Password: $_password');
+    widget.doLogin();
+  }
+
+  @override
+  void dispose() {
+    //Get rid of controllers and nodes when they're not used any more
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  //TODO: Consider implementing a widget for returning InputDecoration to clean up the decorations?
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: signIn(
+            menuStyle, _emailController, _passwordController, _checkLogin),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login pages/sign_in.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+Widget signIn(Color menuStyle, TextEditingController emailController,
+    TextEditingController passwordController, void Function() checkLogin) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      TextField(
+        controller: emailController,
+        decoration: InputDecoration(
+          labelText: 'Email',
+          labelStyle: TextStyle(color: menuStyle),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: menuStyle),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: menuStyle),
+          ),
+        ),
+        style: TextStyle(color: menuStyle),
+      ),
+      const SizedBox(height: 16.0),
+      TextField(
+        controller: passwordController,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          labelStyle: TextStyle(color: menuStyle),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: menuStyle),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: menuStyle),
+          ),
+        ),
+        style: TextStyle(color: menuStyle),
+        obscureText: true,
+      ),
+      const SizedBox(height: 16.0),
+      ElevatedButton(
+        onPressed: checkLogin,
+        child: const Text('Log In'),
+      ),
+    ],
+  );
+}
