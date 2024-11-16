227990c3f103138c17fdc5954f1fedc667029ecd
Noah Lee <noahjefferson.lee@calbaptist.edu>
2024-11-16 12:39:27 -0800

    Made main provider capable

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,51 @@
 import 'package:csc322_streaker_final/Testing/testing_screen.dart';
 import 'package:csc322_streaker_final/screens/login pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:csc322_streaker_final/navigator.dart';
+import 'package:csc322_streaker_final/providers/items_provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ItemList(), // Initialize the ItemList provider
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.black,
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHome(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  bool isLoggedIn = false;
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
 
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool isLoggedIn = false;
   bool testingEnabled = false; //TODO: Remove this when testing is complete
+  var uid = '';
 
   doLogin() {
     setState(() {
       isLoggedIn = true;
     });
   }
-
-  var uid = '';
 
   updUid(String newUid) {
     setState(() {
@@ -36,17 +55,10 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        primarySwatch: Colors.blue,
-      ),
-      home: testingEnabled
-          ? const TestingScreen() //TODO: Remove this when testing is complete
-          : isLoggedIn
-              ? HomePage(uid: uid)
-              : LoginPage(doLogin: doLogin, changeUid: updUid),
-    );
+    return testingEnabled
+        ? const TestingScreen() //TODO: Remove this when testing is complete
+        : isLoggedIn
+            ? HomePage(uid: uid)
+            : LoginPage(doLogin: doLogin, changeUid: updUid);
   }
 }
