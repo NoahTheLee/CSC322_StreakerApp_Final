0dcb2bafe9a217d95856b54ba4404510ba6781a5
Noah Lee <noahjefferson.lee@calbaptist.edu>
2024-11-17 15:13:19 -0800

    Simple skeleton for tasks and user classes

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+class User {
+  User(
+    this.email,
+    this.password,
+    this.username,
+    this.uid,
+  );
+
+  final String email;
+  final String password;
+  final String username;
+
+  final String uid;
+}
